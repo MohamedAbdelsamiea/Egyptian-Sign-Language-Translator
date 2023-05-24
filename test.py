3330a1e369b4a84c168b7a910826bbeed03d36d9
MohamedAbdelsamiea <38036038+MohamedAbdelsamiea@users.noreply.github.com>
2023-05-24 19:04:47 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-# Create a VideoCapture object
-cap = cv2.VideoCapture(3)
-
-# Check if the camera is opened successfully
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
